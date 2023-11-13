5b1a972c75ae36589ca8ed5c6d078205baa37481
Yohann HERBET <yherbet@perig.com>
2023-11-13 13:29:31 +0100

    alt in pictures, links in social links

--- src/components/home.js
+++ src/components/home.js
@@ -33,15 +33,19 @@ const Home = () => {
     return (
         <>
             <div className="container-mobile-socials d-flex mt-3 mt-md-4">
-                <Img className="mobile-socials" fluid={data.imglogoLinkedin.childImageSharp.fluid} />
-                <Img className="mobile-socials" fluid={data.imgLogoGithub.childImageSharp.fluid} />
+                <a href="https://www.linkedin.com/in/yohannherbet/" target="_blank" rel="noreferrer">
+                    <Img className="mobile-socials" fluid={data.imglogoLinkedin.childImageSharp.fluid} alt="logo linkedin" />
+                </a>
+                <a href="https://github.com/YohannHERBET/" target="_blank" rel="noreferrer">
+                    <Img className="mobile-socials" fluid={data.imgLogoGithub.childImageSharp.fluid} alt="logo github"/>
+                </a>
             </div>
             <div className="w-100 py-4 my-4 py-md-5 my-md-5 d-flex justify-content-center">
                 <div className="photo-container d-flex flex-column align-items-center text-center">
-                    <Img className="img-profil-img" fluid={data.imgPhotoProfil.childImageSharp.fluid} />
+                    <Img className="img-profil-img" fluid={data.imgPhotoProfil.childImageSharp.fluid} alt="image de profil de Yohann" />
                     <h2 className="mt-3">Hey, je suis <span>Yohann Herbet</span>,</h2>
                     <h2>développeur fullstack</h2>
-                    <h5>je place l'écoute et la qualité au coeur de mon travail</h5>
+                    <h5  id="about">je place l'écoute et la qualité au coeur de mon travail</h5>
                 </div>
             </div>
         </>
